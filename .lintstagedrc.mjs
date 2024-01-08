c0d7ca5af901231264989689aa8151e8d00632b4
junners <jose.junryan@gmail.com>
2024-01-07 18:22:27 -0800

    feat: twilio-service initialization

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,10 @@
 export default {
-  "**/*.{cls,cmp,component,css,html,js,json,md,page,trigger,xml,yaml,yml}": ["prettier --write"],
+  "**/*.{cls,cmp,component,css,html,js,json,md,page,trigger,xml,yaml,yml}": [
+    "prettier --write"
+  ],
   "**/{aura,lwc}/**": ["eslint"],
-  "**/*.{cls,page,trigger}": (filenames) => `sf scanner run --engine pmd --target ${filenames.join(",")} --json`
+  "**/*.{cls,page,trigger}": [
+    (filenames) =>
+      `sf scanner run --engine pmd --target ${filenames.join(",")} --json`
+  ]
 };
